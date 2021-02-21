6de8eb2ed16e066da80ff8631bf96f81342b953f
serge160889 <serge_voit_2006@mail.ru>
2021-02-21 13:37:18 +0300

    finish

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -1,6 +1,7 @@
 package academy.belhard;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,11 +9,11 @@ public class Main {
     public static void main(String[] args) {
 
         Person person1 = new Person("Sergei", "Voitehovich", new Addres("Minsk", "Glebki", 28));
-    Person person2 = new Person("Sinica", "Olga", new Addres("Brest", "Moskovskaya", 17));
-    Person person3 = new Person("Vorobey", "Elena", new Addres("Grodno", "Pyshkina", 10));
+    Person person2 = new Person("Olga", "Sinica", new Addres("Brest", "Moskovskaya", 17));
+    Person person3 = new Person("Elena", "Vorobey", new Addres("Grodno", "Pyshkina", 10));
     Person person4 = new Person(null, "Dybovik", new Addres("Vitebsk", "Chkalova", 37));
     Person person5 = new Person("Vladimir", null, new Addres("Mogilev", "Koroleva", 12));
-    Person person6 = new Person("Sokolov", "Andrey", null);
+    Person person6 = new Person("Andrei", "Socolov", null);
 
     ArrayList<Person> personArrayList = new ArrayList<>();
         personArrayList.add(person1);
@@ -22,19 +23,21 @@ public class Main {
         personArrayList.add(person5);
         personArrayList.add(person6);
 
-        personArrayList.stream()
+          personArrayList.stream()
                 .filter(c -> c.getFirstName() != null)
-            .filter(x -> x.getLastName() != null)
-            .filter(y -> y.getAddres() != null)
-             .sorted((o1, o2) -> o1.getAddres().getHouseNumber()-o2.getAddres().getHouseNumber())
-                .map(str->str.toString())
-             .collect(Collectors.toList())
-             .forEach(System.out::println);
+                .filter(x -> x.getLastName() != null)
+                .filter(y -> y.getAddres() != null)
+                .sorted((o1, o2) -> o1.getAddres().getHouseNumber()-o2.getAddres().getHouseNumber())
+                .map(Object::toString)
+                .collect(Collectors.toList())
+                  .forEach(System.out::println);
+
+        }
 
 
     }
 
 
-    }
+
 
 
